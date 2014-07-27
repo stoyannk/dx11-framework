5a00be5b742135ea2f727678cf7e99a76e3cac49
Stoyan Nikolov <stoyannk@gmail.com>
2014-07-27 15:10:13 -0700

    * Added a new vertex type

--- Dx11/Rendering/VertexTypes.h
+++ Dx11/Rendering/VertexTypes.h
@@ -15,6 +15,21 @@ struct PositionVertex
 	DirectX::XMFLOAT3 Position;
 };
 
+struct PositionNormalVertex
+{
+	PositionNormalVertex()
+	{}
+
+	PositionNormalVertex(const DirectX::XMFLOAT3& pos,
+		const DirectX::XMFLOAT3& normal)
+		: Position(pos)
+		, Normal(normal)
+	{}
+
+	DirectX::XMFLOAT3 Position;
+	DirectX::XMFLOAT3 Normal;
+};
+
 struct PositionColorVertex
 {
 	PositionColorVertex()
@@ -53,6 +68,7 @@ struct StandardVertex
 };
 
 extern D3D11_INPUT_ELEMENT_DESC PositionVertexLayout[1];
+extern D3D11_INPUT_ELEMENT_DESC PositionNormalVertexLayout[2];
 extern D3D11_INPUT_ELEMENT_DESC PositionColorVertexLayout[2];
 extern D3D11_INPUT_ELEMENT_DESC PositionTextureVertexLayout[2];
 extern D3D11_INPUT_ELEMENT_DESC StandardVertexLayout[5];
